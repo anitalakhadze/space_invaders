f733218c5692396e6c1e6be3989442f38d0af5f5
atala15 <atala15@freeuni.edu.ge>
2020-06-24 23:12:52 +0400

    Basic functionality of the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,8 @@
-import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
 	# Initialize game and create a screen object.
@@ -11,21 +12,20 @@ def run_game():
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
 	
-	# Make a ship
-	ship = Ship(screen)
+	# Make a ship, a group of bullets and a group of aliens
+	ship = Ship(ai_settings, screen)
+	bullets = Group()
+	aliens = Group()
+	
+	# Create the fleet of aliens
+	gf.create_fleet(ai_settings, screen, ship, aliens)
 	
 	# Start the main loop for the game.
 	while True:
-		# Watch for keyboard and mouse events.
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-				
-		# Redraw the screen during each pass through the loop.
-		screen.fill(ai_settings.bg_color)
-		ship.blitme()
-				
-		# make the most recently drawn screen visible
-		pygame.display.flip()
+		gf.check_events(ai_settings, screen, ship, bullets)
+		ship.update()
+		gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+		gf.update_aliens(ai_settings, aliens)
+		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 		
 run_game()
--- settings.py
+++ settings.py
@@ -6,4 +6,20 @@ class Settings():
 		# screen settings
 		self.screen_width = 1200
 		self.screen_height = 800
-		self.bg_color = (230, 230, 230)
+		self.bg_color = (222, 222, 222)
+		
+		# ship settings
+		self.ship_speed_factor = 1.5
+		
+		# bullet settings
+		self.bullet_speed_factor = 1
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = 60, 60, 60
+		self.bullets_allowed = 3
+		
+		# alien settings
+		self.alien_speed_factor = 1
+		self.fleet_drop_speed = 10
+		# fleet direction of 1 represents right; -1 represents left
+		self.fleet_direction = 1
